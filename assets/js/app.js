4658e5c82b6aafadd92289579590a1c525dba21f
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-03-12 10:04:25 +0100

    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+let parallax = document.querySelector(".parallax")
+
+let parallaxInstance = new Parallax(parallax)
+
+let hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+    document.body.classList.toggle("menu-open");
+})
+
+
+
